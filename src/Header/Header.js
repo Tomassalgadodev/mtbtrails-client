83b98639d440c3806657407986a5be5d17b9f4b8
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-04 11:24:18 -0600

    Structure and style header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './Header.css';
+import headerLogo from '../assets/header-logo.png';
 
 const Header = () => {
     return (
-        <h1>This is the header</h1>
+        <header>
+            <img src={headerLogo} />
+            <div className='nav-button-container'>
+                <a>Explore</a>
+                <a>Community</a>
+                <a>Saved</a>
+                <a>Shop</a>
+            </div>
+            <button>Log in</button>
+        </header>
     )
 }
 
