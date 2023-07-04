d87419a4b5e8e6a43b542fb68184fe0df9d3dfcd
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-04 14:40:08 -0600

    feat: Grab all trails within 25 miles of user

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Homepage.css";
 
 const Homepage = ({ coords, loadingCurrentLocation }) => {
 
     const [formValue, setFormValue] = useState('');
+    const [loadingTrailData, setLoadingTrailData] = useState(true);
+    const [trailData, setTrailData] = useState([]);
 
     const searchTrails = e => {
         e.preventDefault();
@@ -13,6 +15,35 @@ const Homepage = ({ coords, loadingCurrentLocation }) => {
     const handleChange = e => {
         setFormValue(e.target.value);
     }
+
+    const getTrails = async (coords) => {
+
+        const fetchURL = `https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=${coords.lat}&lon=${coords.long}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '23091ce880msh32853f28162dd0bp17dc3fjsn85b6b58e5faa',
+                'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+            }
+        };
+
+        try {
+            const response = await fetch(fetchURL, options);
+            const data = await response.json();
+            setTrailData(data.data);
+            setLoadingTrailData(false);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        if (!loadingCurrentLocation) {
+            getTrails(coords);
+        } else {
+            console.log('loading');
+        }
+    }, [loadingCurrentLocation]);
 
     return (
         <div>
@@ -25,8 +56,14 @@ const Homepage = ({ coords, loadingCurrentLocation }) => {
                     onChange={handleChange} 
                 />
                 <button>Submit</button>
-                {!loadingCurrentLocation && <h1>{coords.lat}</h1>}
-                {loadingCurrentLocation && <h1>Getting current location...</h1>}
+                {!loadingTrailData && 
+                    // Display trail data
+                    <h1>{coords.lat}</h1>
+                }
+                {loadingTrailData && 
+                    // Display loading trail data
+                    <h1>Getting current location...</h1>
+                }
             </form>
         </div>
     )
