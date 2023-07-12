68aac2025f0903526419d1b2497b29992721b35d
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-12 13:19:18 -0600

    feat: Fetch trail data when visiting trail details page

--- src/TrailDetailsPage/TrailDetailsPage.js
+++ src/TrailDetailsPage/TrailDetailsPage.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './TrailDetailsPage.css';
 import { useParams } from "react-router-dom";
+import { getTrail } from "../api";
 
 const TrailDetailsPage = () => {
+
+    const [trailData, setTrailData] = useState({});
+    const [loadingTrailData, setLoadingTrailData] = useState(true);
 
     const params = useParams();
 
     const { id } = params;
+
+    const getTrailData = async (id) => {
+        const trailData = await getTrail(id);
+        setTrailData(trailData);
+        setLoadingTrailData(false);
+    }
+
+    useEffect(() => {
+        getTrailData(id);
+    }, []);
 
     return (
         <h1>Trail Details Page {id}</h1>
