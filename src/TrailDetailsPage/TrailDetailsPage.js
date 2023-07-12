3316233770de499da9fc5829a33fc1fe9b6deee7
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-12 13:25:26 -0600

    feat: Display trail data to page

--- src/TrailDetailsPage/TrailDetailsPage.js
+++ src/TrailDetailsPage/TrailDetailsPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import './TrailDetailsPage.css';
 import { useParams } from "react-router-dom";
 import { getTrail } from "../api";
+import Header from "../Header/Header";
 
 const TrailDetailsPage = () => {
 
@@ -14,7 +15,7 @@ const TrailDetailsPage = () => {
 
     const getTrailData = async (id) => {
         const trailData = await getTrail(id);
-        setTrailData(trailData);
+        setTrailData(trailData.data[0]);
         setLoadingTrailData(false);
     }
 
@@ -23,7 +24,15 @@ const TrailDetailsPage = () => {
     }, []);
 
     return (
-        <h1>Trail Details Page {id}</h1>
+        <div>
+            <Header />
+            <img src={trailData.thumbnail} style={{ width: '100%'}} />
+            <h1>{trailData.name}</h1>
+            <h2>Rating: {trailData.rating}</h2>
+            <h2>Difficulty: {trailData.difficulty}</h2>
+            <h2>Location: {trailData.city}, {trailData.region}</h2>
+            <p>{trailData.description}</p>
+        </div>
     )
 }
 
