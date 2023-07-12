c32b6893f34d75aef7032169aee4cc597374a65b
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-12 14:02:43 -0600

    feat: Display loading page when waiting for trail data

--- src/TrailDetailsPage/TrailDetailsPage.js
+++ src/TrailDetailsPage/TrailDetailsPage.js
@@ -26,12 +26,17 @@ const TrailDetailsPage = () => {
     return (
         <div>
             <Header />
-            <img src={trailData.thumbnail} style={{ width: '100%' }} />
-            <h1>{trailData.name}</h1>
-            <h2>Rating: {trailData.rating}</h2>
-            <h2>Difficulty: {trailData.difficulty}</h2>
-            <h2>Location: {trailData.city}, {trailData.region}</h2>
-            <p>{trailData.description}</p>
+            {loadingTrailData && <h1>Loading...</h1>}
+            {!loadingTrailData &&
+                <>
+                    <img src={trailData.thumbnail} style={{ width: '100%' }} />
+                    <h1>{trailData.name}</h1>
+                    <h2>Rating: {trailData.rating}</h2>
+                    <h2>Difficulty: {trailData.difficulty}</h2>
+                    <h2>Location: {trailData.city}, {trailData.region}</h2>
+                    <p>{trailData.description}</p>
+                </>
+            }
         </div>
     )
 }
