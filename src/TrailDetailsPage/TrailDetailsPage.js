826dd61c8db98c8931432062fc61b9b9bf1a95b8
Tomas Salgado <tomassalgadodev@gmail.com>
2023-07-12 12:58:08 -0600

    feat: Get id url param

--- src/TrailDetailsPage/TrailDetailsPage.js
+++ src/TrailDetailsPage/TrailDetailsPage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import './TrailDetailsPage.css';
+import { useParams } from "react-router-dom";
 
 const TrailDetailsPage = () => {
+
+    const params = useParams();
+
+    const { id } = params;
+
     return (
-        <h1>Trail Details Page</h1>
+        <h1>Trail Details Page {id}</h1>
     )
 }
 
